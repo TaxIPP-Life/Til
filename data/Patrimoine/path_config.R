6353dbecf71150ad12f6d5cc73e0898f0912e7be
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2013-07-17 20:47:46 +0200

    path configurations

---
+++ data/Patrimoine/path_config.R
@@ -0,0 +1,7 @@
+## This file has to be modified to suit your needs and renamed to path_config.R
+
+chem_patr = "C:/Users/Utilisateur/Documents/Data/patrimoine/2010/"
+dest <-"C:/Users/Utilisateur/Documents/Data/R/til/patrimoine/2010/"
+
+
+  
