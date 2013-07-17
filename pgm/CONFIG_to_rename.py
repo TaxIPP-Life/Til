15fde735ad0ad764eb59d361146a17ec39519898
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2013-07-17 15:11:13 +0200

    Config path
    intend to enable installation not on C:

---
+++ pgm/CONFIG_to_rename.py
@@ -0,0 +1,8 @@
+'''
+Config
+Path of repositories
+'''
+
+path_of = 'C:\\openfisca\\'
+path_til_liam = 'C:\\til-liam\\'
+path_til = 'C:\\til\\'
--- pgm/run_til.py
+++ pgm/run_til.py
@@ -2,30 +2,36 @@
 
 # on dit à la fonction main (qui est ce qui est appelé avec f5 ou f6 d'être utilisée avec les paramètres que l'on donne à sys.argv
 
-from src_liam.importer import  file2h5 #je laisse le main parce que je ne veux pas travailler sur le import, je laisse celui de LIAM2
+from CONFIG import path_of, path_til_liam, path_til
+import sys 
+
+sys.path.append(path_til_liam)
+sys.path.append(path_of)
+
+
+from src_liam.importer import file2h5 #je laisse le main parce que je ne veux pas travailler sur le import, je laisse celui de LIAM2
 from src_liam.simulation import Simulation
 
-chemin = 'C:\\til\\'
 #import
 
-fichier = chemin + 'model\\import_retro.yml'
-fichier = chemin + 'model\\import_PatrimoineR.yml'
-fichier= 'C:\\Til-Liam\\tests\\functional\\import.yml'
-#file2h5(fichier)
+fichier = path_til + '\\model\\import_retro.yml'
+fichier = path_til + '\\model\\import_PatrimoineR.yml'
+# fichier= path_til_liam + '\\tests\\functional\\import.yml'
+file2h5(fichier, 
+        input_dir=path_til + '\\data\\Patrimoine\\')
 
 
-fichier= chemin+'Model\\console.yml'
-#fichier= chemin+'Model\\console_retro.yml'
-# fichier= 'C:\\Til-Liam\\tests\\functional\\simulation.yml'
+fichier= path_til + 'Model\\console.yml'
+#fichier=  path_til + '\\Model\\console_retro.yml'
+# fichier= 'path_til + '\\tests\\functional\\simulation.yml'
 
-simulation= Simulation.from_yaml(
-                                 fichier,
-                     input_dir=None,
-                    input_file=None,
-                    output_dir=None,                    
-                    output_file=None)
+# simulation= Simulation.from_yaml( fichier,
+#                      input_dir=None,
+#                     input_file=None,
+#                     output_dir=path_til + '//output',                    
+#                     output_file=None)
 
-simulation.run(False)
+# simulation.run(False)
 
 # import cProfile
 # command = """simulation.run(False)"""
