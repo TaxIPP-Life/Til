da4716554393daec4eaa2bd1a37004f325c78eec
LouisePaulDelvaux <louise.paul.delvaux@gmail.com>
2014-03-10 18:46:21 +0100

    From excel to xml : basic try
    Exemple avec AVTS à partir des barèmes retraite de l'IPP

--- pgm/Pension/Param/utils.py
+++ pgm/Pension/Param/utils.py
@@ -1,7 +1,8 @@
 # -*- coding:utf-8 -*-
-
+import numpy as np
+import pandas as pd
 from xml.dom import minidom
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 def multiple_lists(element, date):
     seuils = []
@@ -51,3 +52,57 @@ class Tree2Object(object):
 
             else:
                 setattr(self,a, Tree2Object(b, default))
+
+def from_excel_to_xml(data, code, data_date, description, xml = 'test_xml', example = False, format = "integer"):
+    ''' Fonction qui transforme une colonne de data en lignes .xml '''
+    def _format_date(date):
+        day = date[0:2]
+        month = date[3:5]
+        year = date[6:]
+        date = year + "-" + month + "-" + day
+        return date
+    
+    with open(xml, "w") as f:
+        to_write = '<CODE description= "' + description + '" code="' + code + '" format="' + format + '">\n'
+        f.write(to_write)
+        # Insertions des paramètres
+        for i in range(len(data)):
+            print i
+            if i == 0 : 
+                fin = "2014-12-01"
+            else:
+                fin = str(datetime.strptime(debut,"%Y-%m-%d").date() - timedelta(days=1))
+            debut = str(data_date[i])[0:10]
+            if example:
+                debut = _format_date(debut)
+                fin = _format_date(fin)
+            to_write ='<VALUE valeur= "' + str(data[i]) + '" deb="' + debut + '" fin="' + fin + '"/>\n'
+            f.write(to_write)
+        to_write = '</CODE>\n'
+        f.write(to_write)
+        # Corps du dofile
+        f.close()
+if __name__ == '__main__': 
+    # Example  
+    example = False
+    if example:      
+        data = [0,2,3,99]
+        date = ["01/04/2013", "01/04/2012", "01/04/2011", "01/04/2010"]
+        code = "test"
+        description = "salut"
+        from_excel_to_xml(data, code, date, description, xml = 'test_xml', example = True)
+        
+    # Example from excel  
+    xlsxfile = pd.ExcelFile('Bareme_retraite.xlsx')
+    
+    # AVTS
+    data = xlsxfile.parse('AVTS_montants (1962-2012)', index_col = None)
+    #print data.ix[1:14, 'date'].to_string()
+    dates = np.array(data.ix[1:13, 'date'])
+    avts = np.array(data.ix[1:13, 'avts'])
+    plaf_avts_seul = np.array(data.ix[1:13, 'plaf_mv_seul'])
+    plaf_avts_couple = np.array(data.ix[1:13, 'plaf_mv_men'])
+    
+    #from_excel_to_xml(data = avts, description = "Montant de l'allocations aux vieux travailleurs salariés", code = "montant", format = "float", data_date = dates)
+    #from_excel_to_xml(data = plaf_avts_seul, description = "Plafond de ressources - personne seul", code = "plaf_seul", format = "float", data_date = dates)
+    from_excel_to_xml(data = plaf_avts_couple, description = "Plafond de ressources - couple", code = "plaf_couple", format = "float", data_date = dates)
