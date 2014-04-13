9dbd9829eb669486a7c6e435f70345234a49f67e
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2014-04-13 22:13:03 +0200

    improve comparaison

--- utilisations/Compar_Destinie_Patrimoine/run.py
+++ utilisations/Compar_Destinie_Patrimoine/run.py
@@ -6,27 +6,27 @@ Les tables issues de Destinie et de Patrimoine doivent déjà avoir été créé
     - data//data//Destinie//Patrimoine.py et data//data//Destinie//Destinie.py
     Pour Patrimoine, ça ne sert à rien de duppliquer pour comparer donc on part de la version Patrimoine_0
 '''
-
 import pdb
 
-from pgm.CONFIG import path_til
+from pgm.CONFIG import path_model
 from pandas import read_hdf, HDFStore
 import tables
 import numpy as np
 from scipy.stats import mstats
 
-path_data = path_til + 'Model\\'
 
 # retourne une ficher stat qui permet la comparaison entre deux tables
 # d'abord on récupéère les stat de chaque table, 
 # ensuite on fait la différence. 
 
-list_var_num = ['agem']
-list_var_qual = ['civilstate']
+list_var_num = ['agem','sali','rsti','choi']
+list_var_qual = ['civilstate','workstate','civilstate','findet','quimen','quifoy','sexe']
 
-pat = read_hdf(path_data + 'Patrimoine_300.h5', 'entities//person')
-dest = read_hdf(path_data + 'Destinie.h5', 'entities//person')
+pat = read_hdf(path_model + 'Patrimoine_400.h5', 'entities//person')
+dest = read_hdf(path_model + 'Destinie.h5', 'entities//person')
 table = pat
+
+
 
 table1 = 'Patrimoine_300'
 table2 = 'Destinie'
@@ -51,11 +51,11 @@ class Comparaison_bases(object):
     def load(self):
         self.tables = []
         for name_table in self.name_tables:
-            table = read_hdf(path_data + name_table + '.h5', 'entities//person')
+            table = read_hdf(path_model + name_table + '.h5', 'entities//person')
             table['Total'] = 'Total'
             self.tables += [table]
                 
-    def get_stat_num(self, list_var_num, sous_cat=None):
+    def get_stat_num(self, var_num, sous_cat=None):
         ''' retourne les stats sur des variables numérique (ou pseudo numériques)
             - les variables sont celle de list_var_num
             - les stats sont celle de la fonction get_stat_on_numeric (que l'on pourrait mettre en attribut de la class)
@@ -67,50 +67,54 @@ class Comparaison_bases(object):
 
         tab0, tab1 = self.tables
 
-        return [tab0[list_var_num + sous_cat].groupby(sous_cat).agg(stat_on_numeric),
-                 tab1[list_var_num + sous_cat].groupby(sous_cat).agg(stat_on_numeric)]
+        return [tab0[[var_num] + sous_cat].groupby(sous_cat).agg(stat_on_numeric),
+                 tab1[[var_num] + sous_cat].groupby(sous_cat).agg(stat_on_numeric)]
        
-    def get_stat_qual(self, list_var_qual, sous_cat=None):
+    def get_stat_qual(self, var_qual, sous_cat=None):
         ''' retourne les stats sur des variables qualitative, en fait la fréquence
             - les variables sont celle de list_var_qual
             - le résultats est un dictionnaire avec une clé pour chaque variable et comme valeur une liste
             de deux tables qui sont les fréquences de la variable.
         '''        
-        output = {}
+
         if sous_cat is None:
             sous_cat = ['Total']
 
         tab0, tab1 = self.tables  
-        gp0 = tab0[list_var_qual+ sous_cat].groupby(sous_cat)
-        gp1 = tab1[list_var_qual+ sous_cat].groupby(sous_cat)
-        for var_qual in list_var_qual:         
-            value0 = gp0[var_qual].value_counts()
-            value0 = 100*value0/value0.sum()
-            value1 = gp1[var_qual].value_counts()
-            value1 = 100*value1/value1.sum()
-            output[var_qual] = [value0, value1]
-        return output
+        gp0 = tab0[[var_qual] + sous_cat].groupby(sous_cat)
+        gp1 = tab1[[var_qual] + sous_cat].groupby(sous_cat)
+
+        value0 = gp0[var_qual].value_counts()
+        value0 = 100*value0/value0.sum()
+        value1 = gp1[var_qual].value_counts()
+        value1 = 100*value1/value1.sum()
+        
+        return value0, value1
     
-    def get_diff_num(self, list_var_num, sous_cat=None):
+    def get_diff_num(self, var_num, sous_cat=None):
         ''' sort dans un dictionnaire la différence entre les statistiques sur les deux tables'''
-        stat0, stat1 = self.get_stat_num(list_var_num, sous_cat)
+        print var_num
+        stat0, stat1 = self.get_stat_num(var_num, sous_cat)
         return stat1 - stat0       
         
-    def get_diff_qual(self, list_var_qual, sous_cat=None):
+    def get_diff_qual(self, var_qual, sous_cat=None):
         ''' sort dans un dictionnaire la différence entre les statistiques sur les deux tables'''
-        stat = self.get_stat_qual(list_var_qual, sous_cat)
-        output = {}
-        for var_qual in stat.keys():
-            output[var_qual] =  stat[var_qual][1] - stat[var_qual][0]
-        return output          
+        stat = self.get_stat_qual(var_qual, sous_cat)
+        return stat[1] - stat[0]        
 
  
             
 if __name__ == '__main__':
-    cc = Comparaison_bases('Patrimoine_300','Destinie')
+    cc = Comparaison_bases('Patrimoine_400','Destinie')
     cc.load()
     tab0, tab1 = cc.tables
-    print cc.get_diff_num(list_var_num, ['sexe'])
-    print cc.get_diff_qual(list_var_qual, ['sexe'])
+    for var_num in list_var_num:
+        if var_num not in ['sexe']:
+            print cc.get_diff_num(var_num, ['sexe'])
+    for var_qual in list_var_qual:
+        if var_qual not in ['sexe']:
+            print var_qual
+            print cc.get_diff_qual(var_qual, ['sexe'])
     self = cc
+    cc.get_stat_qual('findet', ['sexe'])
     pdb.set_trace()
