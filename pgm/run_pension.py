a77fff42a07fe0a5f2d7fa49b45ef09b7c113ef1
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2014-06-19 00:54:55 +0200

    modernisation of run_pension

--- pgm/run_pension.py
+++ pgm/run_pension.py
@@ -11,6 +11,7 @@ from CONFIG import path_pension
 sys.path.append(path_pension)
 
 from pension_data import PensionData
+from pension_legislation import PensionParam, PensionLegislation
 from simulation import PensionSimulation
 
 def run_pension(context, yearleg, time_step='year', to_check=False, cProfile=False):
@@ -26,17 +27,25 @@ def run_pension(context, yearleg, time_step='year', to_check=False, cProfile=Fal
     info_ind = pd.DataFrame({'id':context['id'], 'agem': context['agem'],'naiss': naiss, 'sexe' : context['sexe'], 
                               'nb_born': context['nb_born'], 'nb_pac': context['nb_pac']})
     #print (context.keys())
-    info_ind  = info_ind.loc[(info_ind['agem'] > 708), :] # 708 = 59 *12
+    info_ind  = info_ind.loc[(info_ind['agem'] > 10), :] # 708 = 59 *12
     info_ind.set_index('id', inplace=True)
     workstate = workstate.loc[workstate['id'].isin(info_ind.index), :]
     workstate.set_index('id', inplace=True)
+    workstate.sort_index(inplace=True)
     sali = sali.loc[sali['id'].isin(info_ind.index), :]
     sali.set_index('id', inplace=True)
-    data = PensionData.from_arrays(workstate, sali, info_ind)
+    sali.sort_index(inplace=True)
+    sali.fillna(0, inplace=True)
     
-    simul_til = PensionSimulation(data)
+    yearleg = context['period']//100
+    if yearleg > 2009: #TODO: remove
+        yearleg = 2009
+    data = PensionData.from_arrays(workstate, sali, info_ind)
+    param = PensionParam(yearleg, data)
+    legislation = PensionLegislation(param)
+    simul_til = PensionSimulation(data, legislation)
     if cProfile:
-        result_til_year = simul_til.profile_main(yearleg, to_check=to_check)
+        result_til_year = simul_til.profile_evaluate(yearleg, to_check=to_check)
     else:
         result_til_year = simul_til.main(yearleg, to_check=to_check)
     return result_til_year
