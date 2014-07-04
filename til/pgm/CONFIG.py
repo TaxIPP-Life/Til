40e94ba7bbf9d3aff7708544f8fe9153917a4559
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2014-07-04 16:59:14 +0200

    Creation of til directory. Will contain python code

---
+++ til/pgm/CONFIG.py
@@ -0,0 +1,14 @@
+'''
+Config
+Path of repositories
+
+'''
+
+path_liam = "/home/benjello/til/liam2"
+path_til = "/home/benjello/til/Til"
+path_pension = "/home/benjello/til/Til-Pension"
+path_model = "/home/benjello/til/Til-BaseModel"
+
+path_data_patr = None
+path_data_destinie = None
+path_data_destinie_old = None
