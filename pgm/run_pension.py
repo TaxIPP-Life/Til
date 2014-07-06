5317d0e5ece11dbc7edd2d2d4b5835e1d093300c
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2014-07-06 11:30:42 +0200

    small improvements

--- pgm/run_pension.py
+++ pgm/run_pension.py
@@ -19,15 +19,21 @@ def run_pension(context, yearleg, time_step='year', to_check=False, output='pens
         - note there is a selection '''
     sali = context['longitudinal']['sali']
     workstate = context['longitudinal']['workstate']
-    # Pour l'instant selection a ce niveau des individus ayant plus de 60 ans pour lancer le calcul des retraites
+    # calcul de la date de naissance au bon format
     datesim = context['period']
-    datesim = dt.date(datesim//100, datesim % 100, 1)
-    naiss = [datesim - relativedelta(months=x) for x in context['agem']]
+    age_year = context['agem'] // 12
+    age_month = context['agem'] % 12 + 1
+    naiss_year = datesim // 100 - age_year
+    naiss_month = datesim % 100 - age_month + 1
+    naiss = pd.Series(naiss_year*100 + naiss_month)
+    naiss = naiss.map(lambda t: dt.date(t // 100, t % 100, 1))
+
     info_ind = pd.DataFrame({'id':context['id'], 'agem': context['agem'],'naiss': naiss, 'sexe' : context['sexe'], 
                               'nb_enf': context['nb_enf'], 'nb_pac': context['nb_pac'], 'nb_enf_RG': context['nb_enf_RG'],
                               'nb_enf_RSI': context['nb_enf_RSI'], 'nb_enf_FP': context['nb_enf_FP'], 'tauxprime': context['tauxprime']})
     info_ind.set_index('id', inplace=True)
     
+    # TODO: filter should be done in liam
     if output == 'dates_taux_plein':
         # But: déterminer les personnes partant à la retraite avec préselection des plus de 55 ans
         #TODO: faire la préselection dans Liam
@@ -53,7 +59,7 @@ def run_pension(context, yearleg, time_step='year', to_check=False, output='pens
     if cProfile:
         result_til_year = simul_til.profile_evaluate(yearleg, to_check=to_check, output=output)
     else:
-        result_til_year = simul_til.main(yearleg, to_check=to_check, output=output)
+        result_til_year = simul_til.evaluate(yearleg, to_check=to_check, output=output)
     if output == 'dates_taux_plein':
         # Renvoie un dictionnaire donnant la date de taux plein par régime (format numpy) et l'index associé
         return result_til_year
