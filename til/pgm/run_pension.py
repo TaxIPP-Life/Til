f368d1176d1557e8088a6d6ef9b03eeed87344b3
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2014-09-14 13:53:46 +0200

    new run_pension

--- til/pgm/run_pension.py
+++ til/pgm/run_pension.py
@@ -13,10 +13,8 @@ from til_pension.pension_legislation import PensionParam, PensionLegislation
 from til_pension.simulation import PensionSimulation
 from utils import output_til_to_liam
 
-
-def run_pension(context, yearleg, time_step='year', to_check=False, output='pension', cProfile=False):
-    ''' run PensionSimulation after having converted the liam context in a PenionData
-        - note there is a selection '''
+def get_pension(context, yearleg):
+    ''' return a PensionSimulation '''
     sali = context['longitudinal']['sali']
     workstate = context['longitudinal']['workstate']
     # calcul de la date de naissance au bon format
@@ -31,14 +29,6 @@ def run_pension(context, yearleg, time_step='year', to_check=False, output='pens
                               'nb_enf_all': context['nb_enf'], 'nb_pac': context['nb_pac'], 'nb_enf_RG': context['nb_enf_RG'],
                               'nb_enf_RSI': context['nb_enf_RSI'], 'nb_enf_FP': context['nb_enf_FP'], 'tauxprime': context['tauxprime']})
     info_ind = info_ind.to_records(index=False)
-    # TODO: filter should be done in liam
-    if output == 'dates_taux_plein':
-        # But: déterminer les personnes partant à la retraite avec préselection des plus de 55 ans
-        #TODO: faire la préselection dans Liam
-        info_ind = info_ind[(info_ind['agem'] > 55 * 12)]
-
-    if output == 'pension':
-        info_ind = info_ind[context['to_be_retired']] #TODO: filter should be done in yaml
 
     workstate = workstate.loc[workstate['id'].isin(info_ind.index), :].copy()
     workstate.set_index('id', inplace=True)
@@ -47,22 +37,10 @@ def run_pension(context, yearleg, time_step='year', to_check=False, output='pens
     sali.set_index('id', inplace=True)
     sali.sort_index(inplace=True)
     sali.fillna(0, inplace=True)
-    yearleg = context['period'] // 100
-    if yearleg > 2009: #TODO: remove
-        yearleg = 2009
+    
     data = PensionData.from_arrays(workstate, sali, info_ind)
     param = PensionParam(yearleg, data)
     legislation = PensionLegislation(param)
-    simul_til = PensionSimulation(data, legislation)
-    if cProfile:
-        result_til_year = simul_til.profile_evaluate(yearleg, to_check=to_check, output=output)
-    else:
-        result_til_year = simul_til.evaluate(yearleg, to_check=to_check, output=output)
-    if output == 'dates_taux_plein':
-        # Renvoie un dictionnaire donnant la date de taux plein par régime (format numpy) et l'index associé
-        return result_til_year
-    elif output == 'pension':
-        result_to_liam = output_til_to_liam(output_til=result_til_year,
-                                            index_til=info_ind.index,
-                                            context_id=context['id'])
-        return result_to_liam.astype(float)
+    simulation = PensionSimulation(data, legislation)
+    simulation.set_config()  
+    return simulation
