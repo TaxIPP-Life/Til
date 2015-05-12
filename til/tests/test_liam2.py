9c58643e418d0b88a86c2d0e26083d88153a50c1
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2015-05-12 19:14:29 +0200

    Improve tests to be used by nosetests

---
+++ til/tests/test_liam2.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+
+import os
+import pkg_resources
+
+
+from liam2.simulation import Simulation
+
+
+def test_liam2_demo_files():
+    liam2_demo_directory = os.path.join(
+        pkg_resources.get_distribution('liam2').location,
+        '..',
+        'tests',
+        'examples'
+        )
+    excluded_files = [
+        'demo_import.yml',  # non working example
+        'demo02.yml',  # TODO: pb with figures
+        ]
+    yaml_files = [os.path.join(liam2_demo_directory, _file) for _file in os.listdir(liam2_demo_directory)
+        if os.path.isfile(os.path.join(liam2_demo_directory, _file))
+        and _file.endswith('.yml')
+        and _file not in excluded_files]
+    print yaml_files
+    for yaml_file in yaml_files:
+        print yaml_file
+        simulation = Simulation.from_yaml(
+            yaml_file,
+            input_dir = os.path.join(liam2_demo_directory),
+            # input_file = input_file,
+            output_dir = os.path.join(os.path.dirname(__file__), 'output'),
+            # output_file = output_file,
+            )
+        simulation.run(False)
